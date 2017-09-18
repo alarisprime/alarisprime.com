92fd48b05c6c59453c930a5125efca3a1da58462
Saneef Ansari <hello@saneef.com>
2017-09-18 19:17:46 +0530

    feat(fonts): replaces Skolar with ET book

--- scripts/webfonts.js
+++ scripts/webfonts.js
@@ -1,11 +1,17 @@
-const WebFont = require('webfontloader');
+const FontFaceObserver = require('fontfaceobserver');
+
+const baseFontFamily = 'et-book';
+const timeout = 6000;
+const fontCheckRegular = new FontFaceObserver(baseFontFamily, {weight: 'normal'}).load(null, timeout);
+const fontCheckBold = new FontFaceObserver(baseFontFamily, {weight: 'bold'}).load(null, timeout);
 
 const initWebFonts = () => {
-	WebFont.load({
-		typekit: {
-			id: 'xsv8mqb'
-		}
-	});
+	Promise.all([fontCheckRegular, fontCheckBold])
+		.then(() => {
+			document.documentElement.className += ' base-fonts-loaded';
+		}, () => {
+			document.documentElement.className += ' base-fonts-loading-expired';
+		});
 };
 
 export default initWebFonts;
