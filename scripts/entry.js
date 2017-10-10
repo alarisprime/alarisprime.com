09e0b307b2455b4b6abcd7039d13b206c619c896
Saneef Ansari <hello@saneef.com>
2017-10-11 05:08:33 +0530

    chore(js): removes unused code

--- scripts/entry.js
+++ scripts/entry.js
@@ -1,7 +1,5 @@
 import Turbolinks from 'turbolinks';
 import initWebFonts from './webfonts';
-import initOverlayContactForm from './overlay-contact-form';
 
 initWebFonts();
 Turbolinks.start();
-initOverlayContactForm();
